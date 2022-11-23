6d61c47f2bd4a17f796c618feada3ec0537cd0c0
stephanebouche <bouche.stephane@yahoo.fr>
2022-11-23 23:55:31 +0100

    Update test.ts

--- workflow/test.ts
+++ workflow/test.ts
@@ -2,16 +2,16 @@ console.log("Hello World!");
 a = 3;
 function sayHello(name: String) {
     if(a == 5){
-        console.log(`Hello ${name}`);
+        console.log(Hello {name}`);
         if(a == 4){
-            console.log(`Heloo ${name}`);
+            console.log(`Heloo ${name}`)
         }
     }
     else if( a != 5){
         console.log(`Hello ${name}`);
     }
     else{
-    console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`)..
         
     }
 }
