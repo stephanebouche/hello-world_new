ec7dde27b3a233ba03f30529dad0686ea25f8dd3
stephanebouche <bouche.stephane@yahoo.fr>
2022-11-23 23:40:37 +0100

    Update test.ts

--- workflow/test.ts
+++ workflow/test.ts
@@ -2,6 +2,8 @@ console.log("Hello World!");
 
 function sayHello(name: String) {
     console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
 }
 
 sayHello("Tom");
