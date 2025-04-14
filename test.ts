b99b889445516cd9e061741d28abe7f4486a2674
Stephane Bouche <bouche.stephane@yahoo.fr>
2025-04-14 09:56:09 +0200

    Improved some code

---
+++ test.ts
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+a = 3;
+b= "http://github.com/stephanebouche/hello-world_new/settings";
+function sayHello(name: String) {
+    if(a < 5){
+        console.log(Hello {name}`)
+       if(a == 4){
+            console.log(`sdgvsdfgsdfgdfHeloo {name}`)}
+        }
+    }
+    if( a < 4){
+        console.log(`Hella ${name}`);
+        if(a == 4){
+            console.log(`Heloo ${name}`)}
+    }
+    else{
+    console.log(`Hello ${name}`)..
+        
+    }
+}
+
+sayHello("Tom");
